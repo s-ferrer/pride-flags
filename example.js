c8867a877775b0cde72fbe336ac75131dfc23bd9
s-ferrer <saraferrer@gmail.com>
2022-02-27 22:14:48 +0100

    Create example file

---
+++ example.js
@@ -0,0 +1,7 @@
+// render pride flag as html image
+
+// Import the library
+const prideFlag = require("pride-flags");
+
+// Call the library by the choosing name provided in the README file
+prideFlags("asexual");
