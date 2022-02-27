f05d891c477b776d162ca08535d360b641701e64
s-ferrer <saraferrer@gmail.com>
2022-02-27 22:14:18 +0100

    Create prideFlags function

---
+++ index.js
@@ -0,0 +1,29 @@
+function prideFlags(name) {
+  const flags = {
+    agender: "./flags/agender.png",
+    aromantic: "./flags/aromantic.png",
+    asexual: "./flags/asexual.png",
+    bisexual: "./flags/bisexual.png",
+    demiromantic: "./flags/demiromantic.png",
+    genderfluid: "./flags/genderfluid.png",
+    genderqueer: "./flags/genderqueer.png",
+    grayAsexual: "./flags/gray-asexual.png",
+    grayromantic: "./flags/grayromantic.png",
+    intersex: "./flags/intersex.png",
+    lesbian: "./flags/lesbian.png",
+    morecolor: "./flags/morecolor.png",
+    nonBinary: "./flags/agender.png",
+    pansexual: "./flags/pansexual.png",
+    polyamory: "./flags/polyamory.png",
+    rainbow: "./flags/rainbow.png",
+    transgender: "./flags/transgender.png",
+  };
+  const flagDivision = document.createElement("div");
+  flagDivision.id = "flag";
+  const flag = document.body.appendChild(flagDivision);
+  flag.innerHTML = `<img src=${flags[`${name}`]} alt=${name}>`;
+
+  return flag;
+}
+
+module.exports = prideFlags;
